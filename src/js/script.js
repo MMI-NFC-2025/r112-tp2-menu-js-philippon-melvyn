3a9efb8e448bd29bf1578d16ee431acc2052418f
philippon-melvyn <melvyn.philippon@edu.univ-fcomte.fr>
2025-11-11 22:30:08 +0100

    commit ouverture menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,10 @@
 
+const toggle = document.querySelector(".menu-btn");
+const nav = document.querySelector("nav.menu");   
+
+toggle.addEventListener("click", () => {
+  nav.setAttribute("aria-hidden", "false"); 
+  toggle.setAttribute("aria-expanded", "true"); 
+});
+
 
